3ad8a929f18415740f364dd581dda65794324616
Esi93 <agneskwakye0.com>
2021-09-17 15:48:11 +0000

    final gallery app project

---
+++ lib/userData.dart
@@ -0,0 +1,23 @@
+List userData = [
+  {
+    "userName": "Mac_gees",
+    "userLocation": "Ankaful prisons",
+    "userProfileImage": "images/profile_4.jfif",
+    "userPost": "images/picture_1.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Mac_gees",
+    "userLocation": "Ankaful prisons",
+    "userProfileImage": "images/profile_2.jfif",
+    "userPost": "images/picture_2.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Mac_gees",
+    "userLocation": "Ankaful prisons",
+    "userProfileImage": "images/profile_3.jfif",
+    "userPost": "images/pictures_3.jfif",
+    "userPostLikes": "23",
+  }
+];
